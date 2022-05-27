ae0dfdd017b4a3326ecfe5db0e4c6d4b7ed2f8cd
Asgator7 <61560623+Asgator7@users.noreply.github.com>
2022-05-26 21:29:41 -0300

    feat - login

---
+++ flutter_project/lib/screens/login.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({Key? key}) : super(key: key);
+
+  @override
+  State<LoginWidget> createState() => LoginWidgetState();
+}
+
+class LoginWidgetState extends State<LoginWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 50.0),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'Login',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Usuário',
+                          labelStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                          hintText: 'Usuário',
+                          hintStyle: TextStyle(
+                              fontSize: 10.0,
+                              color: Color.fromARGB(164, 255, 255, 255)),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        ),
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        validator: (value) {
+                          if (value?.isEmpty == null) {
+                            return 'Usuário inválido';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Senha',
+                          labelStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                          hintText: 'Senha',
+                          hintStyle: TextStyle(
+                              fontSize: 10.0,
+                              color: Color.fromARGB(164, 255, 255, 255)),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        ),
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value?.isEmpty == null) {
+                            return 'Senha inválida';
+                          }
+                          return null;
+                        },
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 30.0),
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 104, 66, 255),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                          child: const Text('Entrar'),
+                          onPressed: () {
+                            // if (_formKey.currentState.validate()) {
+                            //   Navigator.pushReplacementNamed(
+                            //     context,
+                            //     '/home',
+                            //   );
+                            // }
+                          },
+                        ),
+                      ),
+                      //fazer um link para caso não tenha cadastro
+                      const Text(
+                        'Ainda não tem cadastro?',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 104, 66, 255),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                        child: const Text('Cadastre-se'),
+                        onPressed: () {
+                          // if (_formKey.currentState.validate()) {
+                          //   Navigator.pushReplacementNamed(
+                          //     context,
+                          //     '/home',
+                          //   );
+                          // }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
